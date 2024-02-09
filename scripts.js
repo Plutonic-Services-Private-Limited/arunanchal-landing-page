851956288785461cc423ad09eec5884d397a8438
Shivam <shivam@plutonic.co.in>
2024-02-09 13:12:46 +0530

    content added

---
+++ scripts.js
@@ -0,0 +1,11 @@
+
+window.onload = function (){
+    // const anime = import('animejs');
+    anime({
+        targets: '.white36semibold input',
+        value: [0, 1000],
+        round: 1,
+        easing: 'easeInOutExpo'
+    });
+
+}
