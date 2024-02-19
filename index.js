b55a55162e75a90954abf13e691f48f0538164f1
Shivam <shivam@plutonic.co.in>
2024-02-19 16:02:10 +0530

    animations added

--- index.js
+++ index.js
@@ -13,6 +13,22 @@ const observer2 = new IntersectionObserver(intersections => {
     threshold: 0
 });
 
+
+function counter(id, start, end, duration, plus = false) {
+    let obj = document.getElementById(id),
+        current = start,
+        range = end - start,
+        increment = end > start ? 1 : -1,
+        step = Math.abs(Math.floor(duration / range)),
+        timer = setInterval(() => {
+            current += increment;
+            obj.textContent = current + (plus ? "+" : "");
+            if (current == end) {
+                clearInterval(timer);
+            }
+        }, step);
+}
+
 const observer3 = new IntersectionObserver(intersections => {
     intersections.forEach(({
                                target,
@@ -22,6 +38,24 @@ const observer3 = new IntersectionObserver(intersections => {
         target.classList.toggle('animate__fadeInRight', isIntersecting);
         if (isIntersecting) {
             observer3.unobserve(target);
+        }
+    });
+}, {
+    threshold: 0
+});
+
+const observer4 = new IntersectionObserver(intersections => {
+    intersections.forEach(({
+                               target,
+                               isIntersecting
+                           }) => {
+        counter("count1", 0, 45, 1500, true);
+        counter("count2", 0, 85, 1500, false);
+        counter("count3", 0, 120, 1500, true);
+        counter("count4", 0, 300, 1500, true);
+        counter("count5", 5800, 6000, 1500, true);
+        if (isIntersecting) {
+            observer4.unobserve(target);
         }
     });
 }, {
@@ -43,5 +77,8 @@ window.onload = function () {
     });
     document.querySelectorAll('.block-6').forEach(div => {
         observer2.observe(div);
+    });
+    document.querySelectorAll('.block-7').forEach(div => {
+        observer4.observe(div);
     });
 }
